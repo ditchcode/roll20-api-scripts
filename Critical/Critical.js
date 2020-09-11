aea21bd1c432bdd9287b898755897424c8c6ac63
REX HENDERSON <rex@codegratis.com>
2020-09-11 16:54:58 -0500

    Update Critical.js
    Added an "extra" column for redundant text to be added for the slash blunt and pierce

--- Critical/Critical.js
+++ Critical/Critical.js
@@ -12,13 +12,25 @@ var Critical = (function()
 
 	const criticalHit =
 	[
-		{low: 1,  high: 30,  result: "Hard Hit", slash: "2x Damage.", blunt: "2x Damage.", pierce: "2x Damage"},
+		{low: 1,  high: 25,  result: "Hard Hit", slash: "2x Damage.", blunt: "2x Damage.", pierce: "2x Damage"},
+		{low: 26,  high: 30,  result: "Hard Hit", slash: "2x Damage.", blunt: "2x Damage.", pierce: "2x Damage", extra: "Wakka, wakka! I'm from palatka!"},
 		{low: 31, high: 40,  result: "Powerful Hit", slash: "2x Damage. 30% Chance shield breaks.", blunt: "2x Damage. 30% Chance shield breaks.", pierce: "2x Damage. Roll DEX or be knocked down."},
-		{low: 41, high: 65,  result: "Massive Hit", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage."},
-		{low: 66, high: 69,  result: "Eviscerating Hit", slash: "3x Damage. 55% chance shield breaks", blunt: "3x Damage. 55% chance shield breaks", pierce: "3x Damage. Roll DEX or be knocked down."},
+		{low: 41, high: 57,  result: "Massive Hit", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage."},
+		{low: 57, high: 57,  result: "Massive Hit", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage.", extra: "20% primary weapon ripped away 10 feet random direction"},
+		{low: 58, high: 58,  result: "Massive Hit", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage.", extra: "20% secondary weapon or shield ripped away 10 feet random direction"},
+		{low: 59, high: 59,  result: "Massive Hit", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage.", extra: "20% Chance random equipment ripped off you landing 10 feet random direction"},
+		{low: 60, high: 60,  result: "Massive Hit", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage.", extra: "20% Chance random bag, pouch, or sack ripped open spilling contents"},
+		{low: 61, high: 61,  result: "Staggering Blow", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage.", extra: "Armor piece knocked off, strap cut, belt cut, clothes torn/twisted, lose 2 AC until fixed."},
+		{low: 62, high: 62,  result: "Staggering Blow", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage.", extra: "Zero movement until end of round"},
+		{low: 63, high: 63,  result: "Staggering Blow", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage.", extra: "Stagger into any fire within 5 feet, you are caught ablaze."},
+		{low: 64, high: 64,  result: "Staggering Blow", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage.", extra: "Blurred vision, all rolls at disadvantage until end of next round"},
+		{low: 65, high: 65,  result: "Staggering Blow", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage.", extra: "Staggered 10 feet in random direction and left prone"},
+		{low: 66, high: 67,  result: "Eviscerating Hit", slash: "3x Damage. 55% chance shield breaks", blunt: "3x Damage. 55% chance shield breaks", pierce: "3x Damage. Roll DEX or be knocked down.", extra: "Rock the Casbah!"},
+		{low: 68, high: 68,  result: "Eviscerating Hit", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage.", extra: "Spitting up blood, no verbal casting for rest of round"},
+		{low: 69, high: 69,  result: "Eviscerating Hit", slash: "3x Damage.", blunt: "3x Damage.", pierce: "3x Damage.", extra: "Massive bleeding/hemmorrhaging. Extra 1d4 damage for 1d4 rounds."},
 		{low: 70, high: 70,  result: "Hand", slash: "Slashed open, -1 to hit and damage.", blunt: "Smashed, -1 to hit and damage.", pierce: "Punctured muscle, -1 to hit and damage."},
-		{low: 71, high: 71,  result: "Hand", slash: "Lose 1 finger, -1 to hit and damage.", blunt: "1d4 fingers broken, -1 to hit and damage.", pierce: "Punctured muscle, -1 to hit and damage."},
-		{low: 72, high: 72,  result: "Hand", slash: "Lose 1d4 fingers, -2 to hit and damage.", blunt: "hand broken, -2 to hit and damage.", pierce: "Punctured hand incapacitated, -2 to hit and damage."},
+		{low: 71, high: 71,  result: "Hand", slash: "Lose 1 finger, -1 to hit and damage. Was it a ring finger?", blunt: "1d4 fingers broken, -1 to hit and damage.", pierce: "Punctured muscle, -1 to hit and damage."},
+		{low: 72, high: 72,  result: "Hand", slash: "Lose 1d4 fingers, -2 to hit and damage. Good-bye magic ring?", blunt: "hand broken, -2 to hit and damage.", pierce: "Punctured hand incapacitated, -2 to hit and damage."},
 		{low: 73, high: 73,  result: "Foot", slash: "Slashed open, 1/2 movement.", blunt: "Toe crushed, 1/2 movement.", pierce: "Punctured muscle, 1/2 movement."},
 		{low: 74, high: 74,  result: "Foot", slash: "Lose 1d2 toes, 1/2 movement.", blunt: "Smashed, 1/4 movement.", pierce: "Punctured muscle, 1/2 movement."},
 		{low: 75, high: 75,  result: "Leg", slash: "Slashed open, 1/2 movement.", blunt: "Crushed thigh, 1/2 movement. Roll DEX or fall", pierce: "Punctured thigh, 1/2 movement. Roll DEX or fall."},
@@ -43,8 +55,8 @@ var Critical = (function()
 		{low: 95, high: 96,  result: "Head", slash: "Ear removed, lose helm.", blunt: "Skull hit, stunned 1 round, lose 2d4 INT", pierce: "Eye removed, lose helm."},
 		{low: 97, high: 97,  result: "Head", slash: "Nose removed.", blunt: "Nose crushed.", pierce: "Skull hit, stunned 1 round, lost 1d4 INT."},
 		{low: 98, high: 98,  result: "Head", slash: "1d4 teeth lost.", blunt: "1d4 teeth crushed.", pierce: "Skull hit, stunned 1 round, lost 1d4 INT."},
-		{low: 99, high: 99,  result: "Head", slash: "Nose removed, 1d4 teeth lost.", blunt: "Nose crushed, 1d4 teeth crushed.", pierce: "Skull pierced, dead."},
-		{low: 100, high: 100,  result: "Head", slash: "Decapitated, dead.", blunt: "Skull crushed, dead.", pierce: "Skull pierced, dead."}
+		{low: 99, high: 99,  result: "Head", slash: "Nose removed, 1d4 teeth lost.", blunt: "Nose crushed, 1d4 teeth crushed.", pierce: "Skull pierced, dead.", extra: "That's going to leave a scar."},
+		{low: 100, high: 100,  result: "Head", slash: "Decapitated, dead.", blunt: "Skull crushed, dead.", pierce: "Skull pierced, dead.", extra: "Ooo-raaa!"}
 	];
 
 	function registerEventHandlers()
@@ -96,8 +108,12 @@ var Critical = (function()
 					// Sanity check
 					if (smash)
 					{
+					    let msg = rolled.toString() + "% <b>" + smash.result + "</b><br><i><b>Slash: </b>" + smash.slash + '</i><br><i><b>Blunt: </b>' + smash.blunt + '</i><br><i><b>Pierce: </b>' + smash.pierce + '</i>';
+					    if(smash.extra){
+					        msg = msg + "<br>" + smash.extra;
+					    }
 						// Send the critical result as a formatted string in chat
-						sendChat('Critical Hit', rolled.toString() + "% <b>" + smash.result + "</b><br><i><b>Slash: </b>" + smash.slash + '</i><br><i><b>Blunt: </b>' + smash.blunt + '</i><br><i><b>Pierce: </b>' + smash.pierce + '</i>');
+						sendChat('Critical Hit', msg);
 					}
 					else
 					{
